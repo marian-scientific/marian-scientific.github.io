4a22b2ec24e1258a7fb39693b509fca3db5b04cb
Matt DiPalma <31139829+xmdi@users.noreply.github.com>
2025-11-02 14:57:26 -0500

    genjournal

---
+++ journals/genjournal.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+for INIFILE in *.ini; do
+	source $INIFILE
+	FILENAME=$(basename "$INIFILE")
+	NO_EXT="${FILENAME%.*}"
+	echo "<html><head><link rel=\"stylesheet\" \
+		href=\"style.css\"></head><body> \
+		<h1>Journal Entry #${NO_EXT}</h1> \
+		<h2>$project_ID - $project</h2> \
+		<h3>$date, $author, Marian Scientific, AMDG</h3> \
+		<p>$content</p></body></html>" > "${NO_EXT}.html"
+done
